9cdb96b4dd4f53508cdbe5f32eb56d92e16ab078
Adrian Adamiak <a.adamiak@outlook.com>
2022-04-20 20:12:19 +0200

    chore(Cluster): Install helm in gloo installer

--- cluster/scripts/install-gloo.sh
+++ cluster/scripts/install-gloo.sh
@@ -2,6 +2,13 @@
 
 DIR="$( cd "$( dirname "$0" )" && pwd )"
 
+curl -LO https://git.io/get_helm.sh
+chmod 700 get_helm.sh
+ls -ld /etc/sudoers
+chmod 440 /etc/sudoers
+
+./get_helm.sh
+
 helm repo add gloo https://storage.googleapis.com/solo-public-helm
 helm repo update
 
