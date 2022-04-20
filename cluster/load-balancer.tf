86c25e041a6f8fb0eb245319566469a73dcb214e
Adrian Adamiak <a.adamiak@outlook.com>
2022-04-20 18:34:52 +0200

    chore(Cluster): Fix gloo install script path

--- cluster/load-balancer.tf
+++ cluster/load-balancer.tf
@@ -10,7 +10,7 @@ resource "null_resource" "gloo" {
   depends_on = [null_resource.local_k8s_context]
   provisioner "local-exec" {
     # Update your local gcloud and kubectl credentials for the newly created cluster
-    command = "./scripts/install-gloo.sh"
+    command = "bash ${path.module}/scripts/install-gloo.sh"
   }
 }
 
