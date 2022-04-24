68debb0c301a32ed2e2a761e418fcc0878261bc7
Adrian Adamiak <a.adamiak@outlook.com>
2022-04-24 10:33:59 +0200

    feat(Containers): Show docker registry service email

--- containers/outputs.tf
+++ containers/outputs.tf
@@ -5,3 +5,7 @@ output "workload_identity_pool_provider_id" {
 output "registry_address" {
   value = "${var.region}-docker.pkg.dev/${var.project_name}/${google_artifact_registry_repository.docker.repository_id}"
 }
+
+output "registry_service" {
+  value = google_service_account.docker_registry_admin.email
+}
--- outputs.tf
+++ outputs.tf
@@ -6,3 +6,8 @@ output "docker_registry_address" {
   description = "Full address to the docker registry"
   value       = module.containers.registry_address
 }
+
+output "docker_registry_service" {
+  description = "Email address for the docker registry service"
+  value       = module.containers.registry_service
+}
